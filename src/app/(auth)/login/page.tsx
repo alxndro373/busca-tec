144e864ed4ec91b802d983401af01657468cf71b
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-10-21 22:16:43 -0600

    add validation for login and register page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,23 +1,46 @@
-'use client'
+"use client"
 
+import { loginSchema } from "@/lib/zod"
 import { useRouter } from "next/navigation"
+import { useForm } from "react-hook-form"
+import {z} from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+
 
 export default function Login(){
+
+
+    const form = useForm<z.infer<typeof loginSchema>>({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            email: "",
+            password: ""
+        },
+    })
+
+    const onSubmit = (values: z.infer<typeof loginSchema>) => {
+        console.log(values)
+    }
+
+
     const router = useRouter()
     return (
         <div className="bg-blue-950 h-screen flex flex-col items-center  gap-8">
             <h1 className="font-bold text-white text-4xl mt-20">Iniciar Sesion</h1>
-            <form className="">
+            <form onSubmit={form.handleSubmit(onSubmit)}>
                 <div className="flex flex-col gap-6">
-                <input type="text" placeholder="correo electronico" className="w-96 py-2" />
-                <input type="password" placeholder="contraseña" className="w-96 py-2"  />
+                <input type="text" placeholder="correo electronico" className="w-96 py-2" {...form.register("email")} />
+                {form.formState.errors.email && <span className="text-red-500">{form.formState.errors.email.message }</span>}
+                <input type="password" placeholder="contraseña" className="w-96 py-2" {...form.register("password")} />
+                {form.formState.errors.password && <span className="text-red-500">{form.formState.errors.password.message}</span>}
                 </div>
                 <span className="mb-8 text-white inline-block">¿Olvidaste tu contraseña?</span>
                 <div className="flex flex-col gap-8 items-center">
                     <button className="bg-[#D0D9EC] p-2 w-64">Iniciar Sesion</button>
-                    <button type="button" onClick={() => router.push('/register')} className="bg-[#D0D9EC] p-2 w-72">¿Nuevo? crear una cuenta.</button>
+                    <button type="button" onClick={() => router.push("/register")} className="bg-[#D0D9EC] p-2 w-72">¿Nuevo? crear una cuenta.</button>
                 </div>
             </form>
+            
         </div>
     )
 }
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,20 +1,45 @@
 'use client'
 
+import { registerSchema } from "@/lib/zod"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { useRouter } from "next/navigation"
+import { useForm } from "react-hook-form"
+import {z} from "zod"
 
 export default function Register(){
+
+    const {handleSubmit, register, formState:{errors}} = useForm<z.infer<typeof registerSchema>>({
+        resolver: zodResolver(registerSchema),
+        defaultValues:{
+            user: "",
+            phone: "",
+            email: "",
+            password: "",
+            confirmPassword: ""
+        },
+    })
+
+    const onSubmit = (values: z.infer<typeof registerSchema>) => {
+        console.log(values)
+    }
+
+
     const router = useRouter()
     return (
         <div className="bg-blue-950 h-screen flex flex-col items-center  gap-8">
             <h1 className="font-bold text-white text-4xl mt-20">Registrate</h1>
-            <form className="">
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="flex flex-col gap-6">
-                <input type="text" placeholder="nombre de usuario" className="w-96 py-2" />
-                    <input type="text" placeholder="correo electronico" className="w-96 py-2" />
-                    <input type="text" placeholder="número de telefono" className="w-96 py-2" />
-                    <input type="password" placeholder="contraseña" className="w-96 py-2"  />
-                    <input type="password" placeholder="confirmar contraseña" className="w-96 py-2" />
-                    
+                    <input type="text" placeholder="nombre de usuario" className="w-96 py-2" {...register("user")} />
+                    {errors.user && <span className="text-red-500">{errors.user.message}</span>}
+                    <input type="text" placeholder="correo electronico" className="w-96 py-2" {...register("email")} />
+                    {errors.email && <span className="text-red-500">{errors.email.message}</span>}
+                    <input type="text" placeholder="número de telefono" className="w-96 py-2" {...register("phone")} />
+                    {errors.phone && <span className="text-red-500">{errors.phone.message}</span>}
+                    <input type="password" placeholder="contraseña" className="w-96 py-2" {...register("password")} />
+                    {errors.password && <span className="text-red-500">{errors.password.message}</span>}
+                    <input type="password" placeholder="confirmar contraseña" className="w-96 py-2" {...register("confirmPassword")} />
+                    {errors.confirmPassword && <span className="text-red-500">{errors.confirmPassword.message}</span>}
                 </div>
                 <span className="mb-8 text-white inline-block">Al registrarte aceptas los <strong>Terminos y </strong> <br /> <strong>condiciones y aviso de privacidad</strong> </span>
                 <div className="flex flex-col gap-8 items-center">
