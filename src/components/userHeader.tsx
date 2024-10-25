eac51831437dc463f864c23d26a60b22efc0716a
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-10-25 17:35:57 -0600

    add user session

--- src/components/userHeader.tsx
+++ src/components/userHeader.tsx
@@ -1,20 +1,40 @@
+import { auth, signOut } from "@/auth"
 import Link from "next/link"
 
 
 
-export default function UserHeader(){
+export default async function UserHeader(){
+
+    const session = await auth()
+
     return (
         <header className="pt-4">
             <nav className="mr-4">
-                <ul className="flex justify-end gap-4">
+                <ul className="flex justify-end gap-4 items-center">
                     <li>
                         <Link href={'/objects'}>OBJETOS</Link>
                     </li>
-                    <li>
-                        <Link href={'/login'}>INICIAR SESIOIN</Link>
-                    </li>
+                
+                    {
+                        session ? <>
+                            <li>
+                                <span>Bienvenido: {session.user?.name}</span>
+                            </li>
+                            <form action={async () => {
+                                "use server"
+                                await signOut()
+                            }}>
+                                <button type="submit" className="bg-red-500 py-2 px-4 rounded-md">Cerrar Sesion</button>
+                            </form>
+                        </>
+                        : <Link href={'/login'}>INICIAR SESIOIN</Link>
+                    }
+
+                    
                 </ul>
             </nav>
+            
+
         </header>
     )
 }
