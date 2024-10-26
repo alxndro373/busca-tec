10c90b86b7845de9ad4118bc05ca5fb4a5d89299
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-10-26 01:12:04 -0600

    update schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable,text, varchar} from "drizzle-orm/pg-core";
+import { boolean, date, pgTable,text, varchar} from "drizzle-orm/pg-core";
 
 
 
@@ -15,4 +15,20 @@ export const usuarios = pgTable("usuarios",{
     password: varchar("password", {length: 32})
     .notNull()
 
+})
+
+
+export const objetos = pgTable("objetos", {
+    id_object: text("id_objeto")
+    .primaryKey()
+    .$default(() => crypto.randomUUID()),
+    name_object: text("nombre del objeto")
+    .notNull(),
+    description: text("descripcion")
+    .notNull(),
+    localization: text("localizacion")
+    .notNull(),
+    date: date("fecha_encontrada"),
+    state: boolean("estado_objeto").default(false),
+    image_url: text("imagen")
 })
