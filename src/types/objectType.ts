838ab1b333b08c721b1178abbdb5be511f937ef3
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-10-26 01:09:24 -0600

    add types of objects

---
+++ src/types/objectType.ts
@@ -0,0 +1,22 @@
+
+/*
+export type objectType = {
+    id_object: string
+    name_object: string,
+    description: string ,
+    localization: string
+    state: boolean | null
+    date: string | null
+    image_url: string | null
+}
+
+*/
+
+export type objectType = {
+    id_object: string,
+    name_object: string,
+    description: string ,
+    localization: string,
+    date: string | null
+    state: boolean | null
+}
