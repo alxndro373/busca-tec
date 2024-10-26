cf1d05d62673e9d087cc51be8c648637b637a585
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-10-26 01:10:52 -0600

    add store for objects

---
+++ src/store/objectStore.ts
@@ -0,0 +1,26 @@
+import { getObjects } from "@/actions/objectAction"
+import { objectType } from "@/types/objectType"
+import {create} from "zustand"
+
+
+interface Props  {
+    objects: objectType[]
+    recuperateObjects: () => Promise<void>
+} 
+
+export const objectStore = create<Props>((set) => 
+({
+    objects: [],
+
+
+    recuperateObjects: async () => {
+        const objects = await getObjects()
+        set(state => ({
+            ...state,
+            objects
+        }))
+    },
+
+    
+
+}))
