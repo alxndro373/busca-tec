560ddffe7b28641408ded84d969d8305da1a499f
alxndro373 <josealex2004@hotmail.com>
2024-12-09 09:02:09 -0600

    cambios v8

--- src/app/adminPages/page.tsx
+++ src/app/adminPages/page.tsx
@@ -9,6 +9,10 @@ export default function AdminPage() {
   const { recuperateObjects, getObjectsForAdmin } = objectStore()
   const [searchText, setSearchText] = useState("")
   const [loading, setLoading] = useState(true)
+  const [selectedCategory, setSelectedCategory] = useState("")
+  const [selectedState, setSelectedState] = useState("")
+  const [selectedDate, setSelectedDate] = useState("")
+  const [sortOrder, setSortOrder] = useState("")
 
   useEffect(() => {
     const fetchObjects = async () => {
@@ -22,13 +26,25 @@ export default function AdminPage() {
 
   const objects = getObjectsForAdmin()
 
-  const filteredData = useMemo(() => {
-    const trimmedSearchText = searchText.trim().toLowerCase()
-    return objects.filter((object) =>
-      object.name_object.toLowerCase().includes(trimmedSearchText)
-    )
-  }, [objects, searchText])
 
+  
+  const filteredAndSortedData = useMemo(() => {
+    const filteredData = objects.filter((object) => {
+      const matchSearchText = searchText ? object.name_object.toLowerCase().includes(searchText.toLowerCase()) : true
+      const matchesCategory = selectedCategory ? object.category === selectedCategory : true
+      const matchesState = selectedState ? object.state === selectedState : true
+      const matchesDate = selectedDate ? object.date && object.date.substring(0, 10) === selectedDate : true
+      return matchSearchText && matchesCategory && matchesState && matchesDate
+    })
+
+    if (sortOrder === "asc") {
+      return filteredData.sort((a, b) => a.name_object.localeCompare(b.name_object))
+    } else if (sortOrder === "desc") {
+      return filteredData.sort((a, b) => b.name_object.localeCompare(a.name_object))
+    }
+    return filteredData
+  }, [objects, searchText, selectedCategory, selectedState, selectedDate, sortOrder])
+    
   return (
     <main>
       <h1 className="bg-blue-950 text-white text-4xl font-bold py-14 text-center">Panel de control</h1>
@@ -45,13 +61,57 @@ export default function AdminPage() {
           onChange={(e) => setSearchText(e.target.value)}
           className="p-2 border-2 border-gray-500 w-full md:w-4/12 mb-4 md:mb-0 shadow-md"
         />
+
+        <select
+          className="p-1 border-2 border-gray-500 w-full md:w-2/12 mb-2 md:mb-0 shadow-md bg-white"
+          value={sortOrder}
+          onChange={(e) => setSortOrder(e.target.value)}
+        >
+          <option value="">Ordenar por</option>
+          <option value="asc">Nombre Ascendente</option>
+          <option value="desc">Nombre Descendente</option>
+        </select>
+
+        <select
+          className="p-1 border-2 border-gray-500 w-full md:w-2/12 mb-2 md:mb-0 shadow-md bg-white"
+          value={selectedCategory}
+          onChange={(e) => setSelectedCategory(e.target.value)}
+        >
+          <option value="">Todas las categorías</option>
+          <option value="Accesorios Personales">Accesorios Personales</option>
+          <option value="Documentos y Tarjetas">Documentos y Tarjetas</option>
+          <option value="Electronica">Electrónica</option>
+          <option value="Ropa y Calzado">Ropa y Calzado</option>
+          <option value="Otro">Otro</option>
+        </select>
+
+        <select
+          className="p-1 border-2 border-gray-500 w-full md:w-2/12 mb-2 md:mb-0 shadow-md bg-white"
+          value={selectedState}
+          onChange={(e) => setSelectedState(e.target.value)}
+        >
+          <option value="">Todos los objetos</option>
+          <option value="perdido">Perdido</option>
+          <option value="encontrado">Encontrado</option>
+        </select>
+
+        <div className="w-full md:w-2/12 mt-4 md:mt-0">
+          <label htmlFor="filter-date" className="block md:inline-block">Fecha:</label>
+          <input
+            type="date"
+            id="filter-date"
+            value={selectedDate}
+            onChange={(e) => setSelectedDate(e.target.value)}
+            className="border-2 border-gray-500 p-1 rounded ml-0 md:ml-2 w-full"
+          />
+        </div>
       </div>
 
       {loading ? (
         <Loader />
-      ) : filteredData.length > 0 ? (
+      ) : filteredAndSortedData.length > 0 ? (
         <ObjectsList
-            objects={filteredData}
+            objects={filteredAndSortedData}
             buttonText=""
             option="admin" onObjectChange={function (): void {
               throw new Error("Function not implemented.")
